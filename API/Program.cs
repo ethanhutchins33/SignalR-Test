3067bb3e95ab94c32a73e075167f0613f314b457
Ethan Hutchins <ethanhutchins33@gmail.com>
2023-01-08 01:52:41 +0000

    add a hub and map it

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddSignalR();
 
 var app = builder.Build();
 
@@ -15,6 +16,11 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseEndpoints(endpoints =>
+    {
+        endpoints.MapHub<MyHub>("/myHub");
+    });
 
 app.UseHttpsRedirection();
 
